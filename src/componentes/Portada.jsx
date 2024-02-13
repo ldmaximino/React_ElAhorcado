c59f806a69d7cd756080bc02630bfb9a12a8a985
Leandro Maximino <ldmaximino@gmail.com>
2024-02-13 16:59:40 -0300

    Subir

--- src/componentes/Portada.jsx
+++ src/componentes/Portada.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import caricatura01 from '../assets/caricatura01.png';
 
 export const Portada = () => {
   const navegacion = useNavigate();
@@ -6,7 +7,7 @@ export const Portada = () => {
     <div className="contenedor-portada">
         <h1 className="bienvenidos">BIENVENIDOS</h1>
         <div className="contenedor-portada-imagen">
-          <img src="/src/assets/caricatura01.png" alt="Caricatura" />
+          <img src={caricatura01} alt="Caricatura" />
         </div>
         <p className="nombre-juego">"El Juego del Ahorcado"</p>
         <button className="btn-jugar" onClick={() => navegacion("pregunta")}>Vamos a Jugar</button>
